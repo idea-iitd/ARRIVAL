1335b856d0c5b87458fa4baa6f2495f845c79e96
sarisht <sarisht@users.noreply.github.com>
2019-12-02 12:13:11 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-clang++ -Xpreprocessor -flto -march=native -O3 experiments/runExperiment.cpp
-if [ ! -d "log" ]; then
-  mkdir log
-fi
-if [ ! -d "log/$1" ]; then
-  mkdir log/$1
-fi
-./a.out ../$1/edges.txt ../$1/labels.txt ../$1/attributes.txt ../$1/$2.txt log/$1/$2$4.log $3 $4
