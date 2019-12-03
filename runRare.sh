acdb949a94c17dda0231ce8f89ea7d7b4120e2e0
Sarisht <sarishtwadhwa.1996@gmail.com>
2019-12-03 07:53:57 +0530

    c

--- runRare.sh
+++ runRare.sh
@@ -1,8 +1,8 @@
 cd ../Baselines/GraphRegExp
 javac *.java
-java -Xmx40960 Graphregexp -nogui -Xmx2048m -e ../../gplus/subgraphs/edgeLabel.txt -create ../../gplus/subgraphs/queryedgeLabel.txt >> log.txt&
-java -Xmx40960 Graphregexp -nogui -Xmx -e ../../dblp/subgraphs/edgeLabel.txt -create ../../dblp/subgraphs/queryedgeLabel.txt >> log.txt&
-sleep 3000
+java -Xmx40960 Graphregexp -nogui -e ../../gplus/subgraphs/edgeLabel.txt -create ../../gplus/subgraphs/queryedgeLabel.txt >> log.txt&
+java -Xmx40960 Graphregexp -nogui -e ../../dblp/subgraphs/edgeLabel.txt -create ../../dblp/subgraphs/queryedgeLabel.txt >> log.txt&
+sleep 4600
 search_terms='Graphregexp' 
 kill $(ps aux | grep "$search_terms" | grep -v 'grep' | awk '{print $2}')
 
