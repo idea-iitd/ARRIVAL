15c547dcff66d85620be0e772e11fbbed8221bf3
Sarisht <sarishtwadhwa.1996@gmail.com>
2019-12-05 02:49:20 +0530

    c

--- parseLCR.py
+++ parseLCR.py
@@ -2,9 +2,15 @@ files = ["5_200","10_200","15_200","20_200","25_200","30_200","30_175","30_166",
 
 for f in files:
     fil = open('../'+f+'.csv','r')
-
-    while l:
-        for i in range(4):
-            l = fil.readline()
-        mem = l.split(',')[2]
-    print(f.split('_')[1], f.split('_')[0], mem, 0)
+    for i in range(4):
+        l = fil.readline()
+    mem = l.split(',')[2]
+    for i in range(6):
+        l = fil.readline()
+    try:
+        pos_time = int(l.split(',')[2])
+        l = fil.readline()
+        neg_time = int(l.split(',')[2])
+        print(f.split('_')[1], f.split('_')[0], mem, (pos_time+neg_time)/2)
+    except:
+        print(f.split('_')[1], f.split('_')[0], mem, 0)
